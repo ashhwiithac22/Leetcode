043326a0d4db2e8aeb8614cae3c60209774320f3
Ashwitha C <71762333004@cit.edu.in>
2026-01-08 09:55:29 +0530

    Time: 4 ms (55.57%), Space: 19.2 MB (86.7%) - LeetHub

---
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def removeElements(self, head, val):
+        if head == None:
+            return head
+
+        while head and head.val == val:
+            head = head.next
+        
+        curr = head
+        while curr and curr.next:
+            if curr.next.val == val:
+                curr.next = curr.next.next
+            else:
+                curr = curr.next
+        return head
