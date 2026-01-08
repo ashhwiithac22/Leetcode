1db02b85dccd5435c62f9e36c89edabf4af33758
Ashwitha C <71762333004@cit.edu.in>
2026-01-08 14:57:30 +0530

    Time: 3 ms (39.25%) | Memory: 13.2 MB (51.29%) - LeetSync

---
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def levelOrder(self, root):
+        res = []
+        q = collections.deque()
+        q.append(root)
+        while q:
+            qlen = len(q)
+            level = []
+            for i in range(qlen):
+                node = q.popleft()
+                if node:
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            if level:
+                res.append(level)
+        return res
