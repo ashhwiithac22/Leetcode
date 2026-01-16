3e6edc4b0554d3c6de5fff69ff515224bb8f89c3
Ashwitha C <71762333004@cit.edu.in>
2026-01-16 20:18:59 +0530

    Time: 9 ms (63.8%), Space: 19.3 MB (17.17%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def solve(self, index, total, subset, nums, target, result):
+        if total == target:
+            result.append(subset.copy())  
+            return
+        if total > target or index >= len(nums):
+            return
+        subset.append(nums[index])
+        self.solve(index, total + nums[index], subset, nums, target, result)
+        subset.pop()
+        self.solve(index + 1, total, subset, nums, target, result)
+
+    def combinationSum(self, candidates, target):
+        result = []
+        self.solve(0, 0, [], candidates, target, result)
+        return result
