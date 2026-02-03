55666f8c4b2c1fb919f6f9426b8e529ca3bc8514
Ashwitha C <71762333004@cit.edu.in>
2026-02-03 10:06:24 +0530

    Time: 374 ms (47.06%), Space: 0B (100%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,5 +1,6 @@
 # Write your MySQL query statement below
 select e.name as Employee
 from Employee e
-join Employee m on m.id = e.managerId
+join Employee m
+on e.managerId = m.id
 where e.salary > m.salary;
