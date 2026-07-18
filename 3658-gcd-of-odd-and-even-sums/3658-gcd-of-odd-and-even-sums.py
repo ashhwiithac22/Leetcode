6c01ec66084a5a0318f07f5b715cdc6483a1004e
Ashwitha C <71762333004@cit.edu.in>
2026-07-18 10:08:52 +0530

    Time: 0 ms (100%), Space: 19.2 MB (88.47%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.py
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.py
@@ -3,18 +3,13 @@ class Solution:
         osum = n * n 
         esum = n * (n + 1)
         rem = -1
-        divid = max(osum,esum)
-        divi = min(osum,esum)
-        while rem!=0:
-            rem = divid%divi
-            if rem == 0:
-                gcd = divi
-                break
-            divid=divi
-            divi=rem
-        return divi
-
-
+        dividend = max(osum,esum)
+        divisor = min(osum,esum)
+        while divisor != 0:
+            remainder = dividend % divisor
+            dividend = divisor
+            divisor = remainder
+        return dividend
 
 
         
